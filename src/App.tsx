b052b6f2e63d7a9abb05fd8fe3c7c74c42098f7c
Norbert Niziołek <nizioleque@gmail.com>
2022-11-05 20:33:05 +0100

    Add parsing OBJ file

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Menu from './components/Menu';
 import { AppContext } from './AppContext';
 import useForceRerender from './hooks/useForceRerender';
 import useError from './hooks/useError';
-// import { ObjectData } from './types';
+import { Face, ObjectData, Vertex } from './types';
 
 function App() {
   const canvasContainerRef = useRef<HTMLDivElement>(null);
@@ -29,9 +29,64 @@ function App() {
 
   const { showError, errorText, setErrorText } = useError();
 
-  // const [objectData, setObjectData] = useState<ObjectData>();
+  const [_objectData, setObjectData] = useState<ObjectData>();
   const readFile = (file: File) => {
     console.log(file.name);
+    setObjectData(undefined);
+    parseFile(file);
+  };
+
+  const parseFile = async (file: File) => {
+    const vertices: string[][] = [];
+    const normalVectors: string[][] = [];
+    const faces: string[][] = [];
+    const newObjectData: ObjectData = { faces: [] };
+
+    const readObjectDataString = await file.text();
+    const readObjectData = readObjectDataString.split('\n');
+
+    for (const line of readObjectData) {
+      const lineContent = line.split(' ');
+      switch (lineContent[0]) {
+        case 'v':
+          vertices.push(lineContent);
+          break;
+        case 'vn':
+          normalVectors.push(lineContent);
+          break;
+        case 'f':
+          faces.push(lineContent);
+          break;
+      }
+    }
+
+    console.log(vertices.length, normalVectors.length, faces.length);
+
+    for (const face of faces) {
+      const v1Index = face[1].split('//');
+      const v2Index = face[2].split('//');
+      const v3Index = face[3].split('//');
+
+      const v1 = vertices[parseInt(v1Index[0]) - 1];
+      const v2 = vertices[parseInt(v2Index[0]) - 1];
+      const v3 = vertices[parseInt(v3Index[0]) - 1];
+
+      const vn1 = normalVectors[parseInt(v1Index[1]) - 1];
+      const vn2 = normalVectors[parseInt(v2Index[1]) - 1];
+      const vn3 = normalVectors[parseInt(v3Index[1]) - 1];
+
+      console.log(face, v1, v2, v3);
+      newObjectData.faces.push(
+        new Face([
+          new Vertex(v1, vn1),
+          new Vertex(v2, vn2),
+          new Vertex(v3, vn3),
+        ])
+      );
+    }
+
+    setObjectData(newObjectData);
+    console.log(newObjectData);
   };
 
   useEffect(() => {
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,38 @@
-export {};
+export class ObjectData {
+  faces: Face[];
+
+  constructor(faces: Face[]) {
+    this.faces = faces;
+  }
+}
+
+export class Face {
+  vertices: Vertex[];
+
+  constructor(vertices: Vertex[]) {
+    this.vertices = vertices;
+  }
+}
+
+export class Point3D {
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(vertices: string[]) {
+    this.x = parseFloat(vertices[1]);
+    this.y = parseFloat(vertices[2]);
+    this.z = parseFloat(vertices[3]);
+  }
+}
+
+export class Vertex extends Point3D {
+  vector: Vector;
+
+  constructor(vertices: string[], vector: string[]) {
+    super(vertices);
+    this.vector = new Vector(vector);
+  }
+}
+
+export class Vector extends Point3D {}
